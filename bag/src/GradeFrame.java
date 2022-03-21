a5a75d9139e85d6b9e1d5dac8c0c8104c47cb7e4
sj <100743261+sj319134578@users.noreply.github.com>
2022-03-21 09:43:17 +0800

    Add files via upload

---
+++ bag/src/GradeFrame.java
@@ -0,0 +1,45 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.text.JTextComponent;
+
+public class GradeFrame extends JFrame {
+    private static final long serialVersionUID = 1L;
+    static int n;
+    static int[][] array;
+    public GradeFrame(int array[][],int n){
+        this.n = n;
+        this.array = array;
+        MyCanvas c1 = new MyCanvas();
+        c1.setBackground(new Color(255, 230, 222));
+        add(c1);
+
+
+    }
+   private static class MyCanvas extends Canvas {
+
+       //覆盖JPanel的paint方法
+       // 画坐标轴，画点
+       public void paint(Graphics g) {
+           int i=1;
+           int x;
+           int y;
+           Graphics2D g2 = (Graphics2D) g;// 调用新画图类Graphics2D（强制转化为Graphics2D这个类）
+           g2.drawString(" 价值",50, 50);// 画一个矩形
+           g2.drawString(" 重量",300, 300);// 画一个矩形
+           g2.drawLine(50, 300, 300, 300);// 画一个矩形
+           g2.drawLine(50, 300, 50, 50);// 画一个矩形
+           while (i<=n) {
+               x = 50+array[i][0]*2;//价值
+               y = 300-(array[i][1])*2;//重量
+               g2.fillOval(x,y,5,5);    //画点
+               i++;
+
+           }
+       }
+   }
+}
